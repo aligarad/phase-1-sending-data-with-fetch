2390a51dee11f4e57ae0ed7a56b30b7255480acb
kenhunja <garadcali@gmail.com>
2022-08-30 23:52:53 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your code here
+function submitData(name, email) {
+    const configurationObject = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+    },
+    body: JSON.stringify({
+              name: name,
+              email: email,
+            })
+}
+    return fetch("http://localhost:3000/users", configurationObject)
+        
+        .then(resp => resp.json())
+        .then(data => document.querySelector('body').innerHTML=data.id)
+        .catch(error =>{
+            document.querySelector('body').innerHTML=error.message
+    
+        })
+    
+ }
